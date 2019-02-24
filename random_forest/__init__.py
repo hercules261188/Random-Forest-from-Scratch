674f1db7a60536f6fee6d871cc7dca12d13ca644
tdpetrou <petrou.theodore@gmail.com>
2019-02-23 22:19:23 -0500

    simple

--- random_forest/__init__.py
+++ random_forest/__init__.py
@@ -1,43 +1,47 @@
 import numpy as np
 
 class Node:
-    def __init__(self, X, y):
+
+    def __init__(self, X, y, depth):
         self.left = None
         self.right = None
         self.min_error = np.inf
-        self.split_row_idx = None
         self.split_col_idx = None
+        self.num_rows = X.shape[0]
+        self.num_cols = X.shape[1]
+        self.split_val = None
+        self.depth = depth + 1
         self.split(X, y)
 
     def split(self, X, y):
         if len(X) == 1:
-            return self
-        for i in range(X.shape[1]):
-            X1 = X[:, i]
-            order = np.argsort(X1)
-            X1 = X1[order]
-            y1 = y[order]
-            self.calculate_split_idx(y1, i)
+            return None
+        for i in range(self.num_cols):
+            self.calculate_split_idx(X[:, i], y, i)
         
-        order = np.argsort(X[:, self.split_col_idx])
-        X1 = X[order]
-        y1 = y[order]
-        X_left = X1[:self.split_row_idx]
-        y_left = y1[:self.split_row_idx]
-        self.left = Node(X_left, y_left)
-        X_right = X1[self.split_row_idx:]
-        y_right = y1[self.split_row_idx:]
-        self.right = Node(X_right, y_right)
+        if self.split_col_idx is None:
+            return None
+    
+        X_left = X[self.filt]
+        y_left = y[self.filt]
+        self.left = Node(X_left, y_left, self.depth)
+        X_right = X[~self.filt]
+        y_right = y[~self.filt]
+        self.right = Node(X_right, y_right, self.depth)
 
-    def calculate_split_idx(self, y, var_idx):
-        for i in range(len(y)):
-            y_left = y[:i]
-            y_right = y[i:]
+    def calculate_split_idx(self, X, y, var_idx):
+        for x in X:
+            filt = X <= x
+            y_left = y[filt]
+            y_right = y[~filt]
+            if len(y_right) == 0:
+                continue
             error = ((y_left - y_left.mean()) ** 2).sum() + ((y_right - y_right.mean()) ** 2).sum()
             if error < self.min_error:
                 self.min_error = error
-                self.split_row_idx = i
                 self.split_col_idx = var_idx
+                self.split_val = x
+                self.filt = filt
 
 
 class DecisionTree:
@@ -46,15 +50,10 @@ class DecisionTree:
         self.max_depth = max_depth
 
     def fit(self, X, y):
-        self.tree = Node(X, y)
+        self.tree = Node(X, y, -1)
 
     def predict(self, X):
         pass
     
     def score(self, X):
         pass
-
-    
-                
-
-    
